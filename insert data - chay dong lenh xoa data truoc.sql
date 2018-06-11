310a64d6c8c32964c04683f5c0fcdc5cefeb6683
trungdang97 <35669562+trungdang97@users.noreply.github.com>
2018-06-11 15:05:03 +0700

    Revert "ok"
    This reverts commit 28c4b5276b90600e5a22599f789b1d39b22f0d2b.

--- insert data - chay dong lenh xoa data truoc.sql
+++ insert data - chay dong lenh xoa data truoc.sql
@@ -31,14 +31,14 @@ INSERT [dbo].[TTGiangDay] ([Lop_ID], [GV_ID]) VALUES (3, N'gv003')
 INSERT [dbo].[TTGiangDay] ([Lop_ID], [GV_ID]) VALUES (4, N'gv004')
 
 --xoa toan bo du lieu trong database
-EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL' 
-GO 
+--EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL' 
+--GO 
 
-EXEC sp_MSForEachTable 'DELETE FROM ?' 
-GO 
+--EXEC sp_MSForEachTable 'DELETE FROM ?' 
+--GO 
 
 ---- enable referential integrity again 
-EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL' 
-GO
+--EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL' 
+--GO
 
 --select * from HS
